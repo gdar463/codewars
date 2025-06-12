136d6f7c6442cd424e472cf4fcba8bec9bff9432
gdar463 <gdar463@gmail.com>
2025-06-12 23:39:47 +0200

    finished stringEnds

--- StringEnds/stringEnds.cpp
+++ StringEnds/stringEnds.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <algorithm>
 #include <string>
 
 bool solution(std::string const &str, std::string const &ending) {
-    println("Hello from StringEnds!")
-    return 0;
+  std::string::const_iterator str_end = str.end() - 1;
+  std::string::const_iterator ending_end = ending.end() - 1;
+  for (int i = 0; i < std::min(ending.length(), str.length()); i++) {
+    if (*str_end != *ending_end) {
+      return false;
+    }
+    str_end--;
+    ending_end--;
+  }
+  return true;
 }
