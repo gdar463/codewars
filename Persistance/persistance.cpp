cdab859a980d5a7214d2afe64f75ee482c90baf3
gdar463 <gdar463@gmail.com>
2025-06-18 12:55:24 +0200

    completed persistence

--- Persistance/persistance.cpp
+++ Persistance/persistance.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <string>
 
-int persistence(long long n) {
-  std::cout << "Hello from Persistance!" << std::endl;
-  return 0;
+int calc(long long n, int i) {
+  std::string str = std::to_string(n);
+  if (str.length() == 1) {
+    return i;
+  }
+  int out = 1;
+  for (int j = 0; j < str.length(); j++) {
+    out *= str[j] - '0';
+  }
+  return calc(out, i + 1);
 }
+
+int persistence(long long n) { return calc(n, 0); }
