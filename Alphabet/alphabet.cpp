f1627c755ec3daf955575f2b336ba3a70f91922d
gdar463 <gdar463@gmail.com>
2025-06-18 13:22:50 +0200

    completed alphabet pos

--- Alphabet/alphabet.cpp
+++ Alphabet/alphabet.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
 #include <string>
 
 std::string alphabet_position(const std::string &text) {
-  std::cout << "Hello from Alphabet!" << std::endl;
-  return 0;
+  std::string out = "";
+  for (int i = 0; i < text.length(); i++) {
+    if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+      out +=
+          std::to_string(((text[i] & 0b11011111) ^ 0b100000) - 'a' + 1) + ' ';
+  }
+  return out.substr(0, out.size() - 1);
 }
