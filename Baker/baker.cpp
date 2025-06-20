7d3383f05c9b4aa1bdb576a240e08fcdf2d49980
gdar463 <gdar463@gmail.com>
2025-06-20 14:00:18 +0200

    finshed pete the baker

--- Baker/baker.cpp
+++ Baker/baker.cpp
@@ -1,6 +1,23 @@
+#include <algorithm>
+#include <climits>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 using Ingredients = std::unordered_map<std::string, int>;
+using IngredientPair = std::pair<const std::string, const int>;
 
-int cakes(const Ingredients &recipe, const Ingredients &available) { return 0; }
+int cakes(const Ingredients &recipe, const Ingredients &available) {
+  for (IngredientPair n : recipe) {
+    if (available.find(n.first) == available.end()) {
+      return 0;
+    }
+  }
+  int min = INT_MAX;
+  for (IngredientPair n : available) {
+    if (auto found = recipe.find(n.first); found != recipe.end()) {
+      min = std::min(min, n.second / found->second);
+    }
+  }
+  return min;
+}
