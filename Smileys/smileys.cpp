ca6ea39d993456548e792c34ddea0a5be08983fd
gdar463 <gdar463@gmail.com>
2025-07-06 18:04:58 +0200

    finished Smileys

--- Smileys/smileys.cpp
+++ Smileys/smileys.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
 int countSmileys(std::vector<std::string> arr) {
-  std::cout << "Hello from Smileys!" << std::endl;
-  return 0;
+  int count = 0;
+  for (std::string face : arr) {
+    if (face.front() == ':' || face.front() == ';') {
+      char second = *(face.begin() + 1);
+      if (second == '-' || second == '~') {
+        if (face.back() == ')' || face.back() == 'D') {
+          count++;
+        }
+      } else if (second == ')' || second == 'D') {
+        count++;
+      }
+    }
+  }
+  return count;
 }
