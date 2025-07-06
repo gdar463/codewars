78e2c40ed798cfe452687df5d50e0ad37a132e8a
gdar463 <gdar463@gmail.com>
2025-07-06 14:27:38 +0200

    refactored new project script

---
+++ lib.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+def replace_by_dir(input: str, replacement_patterns: dict[str, str]):
+    for pattern, replace in replacement_patterns.items():
+        input = input.replace(pattern,replace)
+    return input
+
+def sort_out_stuff(signature: str, needs_wrapper: dict[str, tuple[str, ...]], needs_replace: dict[str,str], imports: str):
+    def replace_types(input: str): 
+        for official, temp_name in needs_replace.items():
+            input = input.replace(official, temp_name)
+        return input
+
+    def replace_types_inv(input:str):
+        for official, temp_name in needs_replace.items():
+            input = input.replace(temp_name, official)
+        return input
+
+    template_string = '{{const}}{{type}} {{name}} = {{wrapperStart}}argv[{{index}}]{{wrapperEnd}};'
+    inputs = "\t"
+    call = signature.split("(")[0].split(" ")[1] + "("
+    segments = signature.split("(")[1].split(")")[0].split(", ")
+    for i, val in enumerate(segments):
+        temp = template_string
+        segment = replace_types(val).split(" ")
+        zero_inv = replace_types_inv(segment[0])
+
+        if zero_inv in needs_wrapper:
+            wrapper = needs_wrapper[zero_inv]
+            if segment[0].count("ulonglong") > 0:
+                imports = imports + "#include <string>\n"
+        else:
+            wrapper = tuple(["",""])
+            
+        tempReplacements = {
+            "{{const}}": "const " if "const" in segment else "",
+            "{{type}}": replace_types_inv(segment[1]) if "const" in segment else zero_inv,
+            "{{name}}": segment[-1].replace("&", ""),
+            "{{wrapperStart}}": wrapper[0],
+            "{{wrapperEnd}}": wrapper[1],
+            "{{index}}": str(i + 1)
+        }
+        
+        call += segment[-1].replace("&", "") + (", " if i != len(segments) - 1 else "")
+        temp = replace_by_dir(temp, tempReplacements) + ("\n" if i != len(segments) - 1 else "")
+        inputs += temp
+    call += ")"
+    return inputs, call, imports
--- new-project.py
+++ new-project.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 from sys import platform
+from lib import sort_out_stuff, replace_by_dir
 
 parser = argparse.ArgumentParser("new codewars cpp project")
 parser.add_argument("name")
@@ -39,6 +40,11 @@ for key, val in needs_import.items():
     if signature.find(key) != -1:
         imports = imports + "#include " + val + "\n"
 
+needs_replace = {
+    "unsigned int": "uint",
+    "long long": "longlong",
+    "unsigned long long": "ulonglong"
+}
 needs_wrapper = {
     "int": tuple(["std::atoi(", ")"]),
     "long": tuple(["std::atol(", ")"]),
@@ -47,52 +53,43 @@ needs_wrapper = {
     "double": tuple(["std::atof(", ")"])
 }
 
-template_string = '\t{{const}}{{type}} {{name}} = {{wrapperStart}}argv[{{index}}]{{wrapperEnd}};'
-inputs = ""
-call = signature.split("(")[0].split(" ")[1] + "("
-segments = signature.split("(")[1].split(")")[0].split(", ")
-for i, val in enumerate(segments):
-    temp = template_string if i != 0 else template_string.replace("\t", "")
-    segment = val.replace("unsigned int","uint").replace("unsigned long long", "ulonglong").replace("long long", "longlong").split(" ")
-    temp = temp.replace("{{const}}", "const " if "const" in segment else "")
-    temp = temp.replace("{{type}}", segment[1 if "const" in segment else 0].replace("uint", "unsigned int").replace("ulonglong", "unsigned long long").replace("longlong", "long long"))
-    temp = temp.replace("{{name}}", segment[-1].replace("&",""))
-    if segment[0].replace("uint", "unsigned int").replace("ulonglong", "unsigned long long").replace("longlong", "long long") in needs_wrapper:
-        wrapper = needs_wrapper[segment[0].replace("uint", "unsigned int").replace("ulonglong", "unsigned long long").replace("longlong", "long long")]
-        if segment[0].count("ulonglong") > 0:
-            imports = imports + "#include <string>\n"
-    else:
-        wrapper = tuple(["",""])
-    temp = temp.replace("{{wrapperStart}}", wrapper[0])
-    temp = temp.replace("{{wrapperEnd}}", wrapper[1])
-    call += segment[-1].replace("&", "") + (", " if i != len(segments) - 1 else "")
-    temp = temp.replace("{{index}}", str(i + 1)) + ("\n" if i != len(segments) - 1 else "")
-    inputs += temp
-call += ")"
+inputs, call, imports = sort_out_stuff(signature, needs_wrapper, needs_replace, imports)
 
+bashFileReplacements = {
+    "{{projectName}}": project,
+    "{{projectFile}}": projectLower
+}
 with open("example/example.sh", "rt") as f:
     bashFile = f.read()
-bashFile = bashFile.replace("{{projectName}}", project)
-bashFile = bashFile.replace("{{projectFile}}", projectLower)
+bashFile = replace_by_dir(bashFile, bashFileReplacements)
 
+cppFileReplacements = {
+    "{{project}}": project,
+    "{{signature}}": signature,
+    "{{imports}}": imports
+}
 with open("example/example.cpp", "rt") as f:
     cppFile = f.read()
-cppFile = cppFile.replace("{{project}}", project)
-cppFile = cppFile.replace("{{signature}}", signature)
-cppFile = cppFile.replace("{{imports}}", imports)
+cppFile = replace_by_dir(cppFile, cppFileReplacements)
 
+mainFileReplacements = {
+    "{{imports}}": imports,
+    "{{signature}}": signature + ";",
+    "{{inputs}}": inputs,
+    "{{call}}": call
+}
 with open("example/main.cpp", "rt") as f:
     mainFile = f.read()
-mainFile = mainFile.replace("{{imports}}", imports)
-mainFile = mainFile.replace("{{signature}}", signature + ";")
-mainFile = mainFile.replace("{{inputs}}", inputs)
-mainFile = mainFile.replace("{{call}}", call)
+mainFile = replace_by_dir(mainFile, mainFileReplacements)
 
+mdFileReplacements= {
+    "{{projectName}}": project,
+    "{{officialName}}": official,
+    "{{link}}": link,
+}
 with open("example/README.md", "rt") as f:
     mdFile = f.read()
-mdFile = mdFile.replace("{{projectName}}", project)
-mdFile = mdFile.replace("{{officialName}}", official)
-mdFile = mdFile.replace("{{link}}", link)
+mdFile = replace_by_dir(mdFile, mdFileReplacements)
 
 with open("scripts/" + project + ".sh", "wt") as f:
     f.write(bashFile)
