b5b58f92fb799e5bc55601aac8bbd2346797f461
gdar463 <gdar463@gmail.com>
2025-07-06 17:12:42 +0200

    finished UniqueOrder

--- UniqueOrder/uniqueOrder.cpp
+++ UniqueOrder/uniqueOrder.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<char> uniqueInOrder(const std::string& iterable) {
-  std::cout << "Hello from UniqueOrder!" << std::endl;
+template <typename T>
+std::vector<T> uniqueInOrder(const std::vector<T> &iterable) {
+  std::vector<T> output = {' '};
+  for (T single : iterable) {
+    if (output.back() != single) {
+      output.push_back(single);
+    }
+  }
+  output.erase(output.begin());
+  return output;
+}
+
+std::vector<char> uniqueInOrder(const std::string &iterable) {
+  std::vector<char> output = {' '};
+  for (char single : iterable) {
+    if (output.back() != single) {
+      output.push_back(single);
+    }
+  }
+  output.erase(output.begin());
+  return output;
 }
