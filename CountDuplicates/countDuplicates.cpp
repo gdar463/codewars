2f0a910c7ad00b2a61164c9c44bdb6e5c1825c06
gdar463 <gdar463@gmail.com>
2025-07-06 16:46:15 +0200

    finished CountDuplicates

--- CountDuplicates/countDuplicates.cpp
+++ CountDuplicates/countDuplicates.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <cctype>
 #include <string>
+#include <unordered_map>
 
 std::size_t duplicateCount(const std::string &in) {
-  std::cout << "Hello from CountDuplicates!" << std::endl;
-  return 0;
+  std::size_t count = 0;
+  std::unordered_map<char, int> found;
+  for (int i = 0; i < in.length(); i++) {
+    char curr = std::tolower(in[i]);
+    found.insert_or_assign(
+        curr, found.find(curr) != found.end() ? found.at(curr) + 1 : 1);
+  }
+  for (auto const &x : found) {
+    if (x.second > 1) {
+      count++;
+    }
+  }
+  return count;
 }
