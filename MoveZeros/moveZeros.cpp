6f108498a01d5dbae65a75dc8331068bc2e1e3b8
gdar463 <gdar463@gmail.com>
2025-08-29 13:47:19 +0200

    finished MoveZeros

--- MoveZeros/moveZeros.cpp
+++ MoveZeros/moveZeros.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
 #include <vector>
 
-std::vector<int> move_zeroes(const std::vector<int>& input) {
-  std::cout << "Hello from MoveZeros!" << std::endl;
-  return 0;
+std::vector<int> move_zeroes(const std::vector<int> &input) {
+  std::vector<int> out;
+  int zeros = 0;
+  for (int n : input) {
+    if (n == 0) {
+      zeros++;
+      out.push_back(0);
+    } else {
+      out.emplace(out.end() - zeros, n);
+    }
+  }
+  return out;
 }
