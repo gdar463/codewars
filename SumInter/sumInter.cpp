5ac9198277cb1cec8e6584be58db43d0750eb046
gdar463 <gdar463@gmail.com>
2025-08-29 15:36:17 +0200

    finished SumInter

--- SumInter/sumInter.cpp
+++ SumInter/sumInter.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <algorithm>
 #include <utility>
 #include <vector>
 
 int sum_intervals(std::vector<std::pair<int, int>> intervals) {
-  std::cout << "Hello from SumInter!" << std::endl;
-  return 0;
+  std::sort(intervals.begin(), intervals.end());
+  int out = 0;
+  int start = intervals[0].first;
+  int end = intervals[0].second;
+  for (int i = 1; i < intervals.size(); i++) {
+    int currStart = intervals[i].first;
+    int currEnd = intervals[i].second;
+    if (currStart <= end) {
+      end = std::max(currEnd, end);
+    } else {
+      out += (end - start);
+      start = currStart;
+      end = currEnd;
+    }
+  }
+  out += end - start;
+  return out;
 }
